22a6cbad6602df48320bd2eae519c960a43ae93d
Theodor Truffer <tt@studer-raimann.ch>
2019-08-28 08:29:54 +0200

    stored event: change string -> DomainID, added sleep/wakeup

--- Event/AbstractIlContainerStoredEvent.php
+++ Event/AbstractIlContainerStoredEvent.php
@@ -4,7 +4,8 @@ namespace ILIAS\AssessmentQuestion\CQRS\Event;
 
 use ActiveRecord;
 use ilDateTime;
-use \ilException;
+use ilException;
+use ILIAS\AssessmentQuestion\CQRS\Aggregate\DomainObjectId;
 
 /**
  * Class AbstractIlContainerStoredEvent
@@ -16,48 +17,49 @@ use \ilException;
  * @author  Martin Studer <ms@studer-raimann.ch>
  * @author  Theodor Truffer <tt@studer-raimann.ch>
  */
-abstract class AbstractIlContainerStoredEvent extends ActiveRecord {
+abstract class AbstractIlContainerStoredEvent extends ActiveRecord
+{
 
-	/**
-	 * @var int
-	 *
-	 * @con_is_primary true
-	 * @con_is_unique  true
-	 * @con_has_field  true
-	 * @con_fieldtype  integer
-	 * @con_length     8
-	 * @con_sequence   true
-	 */
-	protected $event_id;
-	/**
-	 * @var string
-	 *
-	 * @con_has_field  true
-	 * @con_fieldtype  text
-	 * @con_length     200
-	 * @con_index      true
-	 * @con_is_notnull true
-	 */
-	protected $aggregate_id;
-	/**
-	 * @var string
-	 *
-	 * @con_has_field  true
-	 * @con_fieldtype  text
-	 * @con_length     200
-	 * @con_index      true
-	 * @con_is_notnull true
-	 */
-	protected $event_name;
-	/**
-	 * @var ilDateTime
-	 *
-	 * @con_has_field  true
-	 * @con_fieldtype  timestamp
-	 * @con_index      true
-	 * @con_is_notnull true
-	 */
-	protected $occurred_on;
+    /**
+     * @var int
+     *
+     * @con_is_primary true
+     * @con_is_unique  true
+     * @con_has_field  true
+     * @con_fieldtype  integer
+     * @con_length     8
+     * @con_sequence   true
+     */
+    protected $event_id;
+    /**
+     * @var string
+     *
+     * @con_has_field  true
+     * @con_fieldtype  text
+     * @con_length     200
+     * @con_index      true
+     * @con_is_notnull true
+     */
+    protected $aggregate_id;
+    /**
+     * @var string
+     *
+     * @con_has_field  true
+     * @con_fieldtype  text
+     * @con_length     200
+     * @con_index      true
+     * @con_is_notnull true
+     */
+    protected $event_name;
+    /**
+     * @var DomainObjectId
+     *
+     * @con_has_field  true
+     * @con_fieldtype  timestamp
+     * @con_index      true
+     * @con_is_notnull true
+     */
+    protected $occurred_on;
     /**
      * @var int
      *
@@ -67,74 +69,79 @@ abstract class AbstractIlContainerStoredEvent extends ActiveRecord {
      * @con_is_notnull true
      */
     protected $container_obj_id;
-	/**
-	 * @var int
-	 *
-	 * @con_has_field  true
-	 * @con_fieldtype  integer
-	 * @con_index      true
-	 * @con_is_notnull true
-	 */
-	protected $initiating_user_id;
-	/**
-	 * @var string
-	 *
-	 * @con_has_field  true
-	 * @con_fieldtype  clob
-	 * @con_is_notnull true
-	 */
-	protected $event_body = '';
+    /**
+     * @var int
+     *
+     * @con_has_field  true
+     * @con_fieldtype  integer
+     * @con_index      true
+     * @con_is_notnull true
+     */
+    protected $initiating_user_id;
+    /**
+     * @var string
+     *
+     * @con_has_field  true
+     * @con_fieldtype  clob
+     * @con_is_notnull true
+     */
+    protected $event_body = '';
 
 
-	/**
-	 * Store event data.
-	 *
-	 * @param string     $aggregate_id
-	 * @param string     $event_name
-	 * @param ilDateTime $occurred_on
-	 * @param int        $initiating_user_id
-	 * @param string     $event_body
-	 */
-	public function setEventData(string $aggregate_id, string $event_name, ilDateTime $occurred_on, int $container_obj_id, int $initiating_user_id, string $event_body) {
-		$this->aggregate_id = $aggregate_id;
-		$this->event_name = $event_name;
-		$this->occurred_on = $occurred_on;
-		$this->container_obj_id  = $container_obj_id;
-		$this->initiating_user_id = $initiating_user_id;
-		$this->event_body = $event_body;
-	}
+    /**
+     * Store event data.
+     *
+     * @param DomainObjectId $aggregate_id
+     * @param string         $event_name
+     * @param ilDateTime     $occurred_on
+     * @param int            $initiating_user_id
+     * @param string         $event_body
+     */
+    public function setEventData(DomainObjectId $aggregate_id, string $event_name, ilDateTime $occurred_on, int $container_obj_id, int $initiating_user_id, string $event_body)
+    {
+        $this->aggregate_id = $aggregate_id;
+        $this->event_name = $event_name;
+        $this->occurred_on = $occurred_on;
+        $this->container_obj_id = $container_obj_id;
+        $this->initiating_user_id = $initiating_user_id;
+        $this->event_body = $event_body;
+    }
 
 
-	/**
-	 * @return int
-	 */
-	public function getEventId(): int {
-		return $this->event_id;
-	}
+    /**
+     * @return int
+     */
+    public function getEventId() : int
+    {
+        return $this->event_id;
+    }
 
 
-	/**
-	 * @return string
-	 */
-	public function getAggregateId(): string {
-		return $this->aggregate_id;
-	}
+    /**
+     * @return DomainObjectId
+     */
+    public function getAggregateId() : DomainObjectId
+    {
+        return $this->aggregate_id;
+    }
 
 
-	/**
-	 * @return string
-	 */
-	public function getEventName(): string {
-		return $this->event_name;
-	}
+    /**
+     * @return string
+     */
+    public function getEventName() : string
+    {
+        return $this->event_name;
+    }
 
 
-	/**
-	 * @return int
-	 */
-	public function getOccurredOn(): ilDateTime {
-		return $this->occurred_on;
-	}
+    /**
+     * @return ilDateTime
+     */
+    public function getOccurredOn() : ilDateTime
+    {
+        return $this->occurred_on;
+    }
 
 
     /**
@@ -145,63 +152,110 @@ abstract class AbstractIlContainerStoredEvent extends ActiveRecord {
         return $this->container_obj_id;
     }
 
-	/**
-	 * @return int
-	 */
-	public function getInitiatingUserId(): int {
-		return $this->initiating_user_id;
-	}
+
+    /**
+     * @return int
+     */
+    public function getInitiatingUserId() : int
+    {
+        return $this->initiating_user_id;
+    }
 
 
-	/**
-	 * @return string
-	 */
-	public function getEventBody(): string {
-		return $this->event_body;
-	}
-
-	//
-	// CRUD
-	//
-	/**
-	 *
-	 */
-	public function create() {
-		parent::create();
-	}
+    /**
+     * @return string
+     */
+    public function getEventBody() : string
+    {
+        return $this->event_body;
+    }
 
 
-	//
-	// Not supported CRUD-Options:
-	//
-	/**
-	 * @throws ilException
-	 */
-	public function store() {
-		throw new ilException("Store is not supported - It's only possible to add new records to this store!");
-	}
+    /**
+     * @param $field_name
+     *
+     * @return int|mixed|string|null
+     */
+    public function sleep($field_name)
+    {
+        switch ($field_name) {
+            case 'occurred_on':
+                return $this->occurred_on->get(IL_CAL_DATETIME);
+            case 'aggregate_id':
+                return $this->aggregate_id->getId();
+            default:
+                return null;
+        }
+    }
 
 
-	/**
-	 * @throws ilException
-	 */
-	public function update() {
-		throw new ilException("Update is not supported - It's only possible to add new records to this store!");
-	}
+    /**
+     * @param $field_name
+     * @param $field_value
+     *
+     * @return ilDateTime|mixed|null
+     * @throws ilDateTimeException
+     */
+    public function wakeUp($field_name, $field_value)
+    {
+        switch ($field_name) {
+            case 'occurred_on':
+                return new ilDateTime($field_value, IL_CAL_DATETIME);
+            case 'aggregate_id':
+                return new DomainObjectId($field_value);
+            default:
+                return null;
+        }
+    }
 
 
-	/**
-	 * @throws ilException
-	 */
-	public function delete() {
-		throw new ilException("Delete is not supported - It's only possible to add new records to this store!");
-	}
+    //
+    // CRUD
+    //
+    /**
+     *
+     */
+    public function create()
+    {
+        parent::create();
+    }
 
 
-	/**
-	 * @throws ilException
-	 */
-	public function save() {
-		throw new ilException("Save is not supported - It's only possible to add new records to this store!");
-	}
+    //
+    // Not supported CRUD-Options:
+    //
+    /**
+     * @throws ilException
+     */
+    public function store()
+    {
+        throw new ilException("Store is not supported - It's only possible to add new records to this store!");
+    }
+
+
+    /**
+     * @throws ilException
+     */
+    public function update()
+    {
+        throw new ilException("Update is not supported - It's only possible to add new records to this store!");
+    }
+
+
+    /**
+     * @throws ilException
+     */
+    public function delete()
+    {
+        throw new ilException("Delete is not supported - It's only possible to add new records to this store!");
+    }
+
+
+    /**
+     * @throws ilException
+     */
+    public function save()
+    {
+        throw new ilException("Save is not supported - It's only possible to add new records to this store!");
+    }
 }

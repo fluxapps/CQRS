4d6848191e956a40ffa726a0dd3a5db1564bccf8
Theodor Truffer <tt@studer-raimann.ch>
2019-09-11 11:28:07 +0200

    changes in abstract event (generalize restore and constructor)

--- src/Event/AbstractDomainEvent.php
+++ src/Event/AbstractDomainEvent.php
@@ -15,64 +15,86 @@ use srag\CQRS\Aggregate\DomainObjectId;
  * @author  Martin Studer <ms@studer-raimann.ch>
  * @author  Theodor Truffer <tt@studer-raimann.ch>
  */
-abstract class AbstractDomainEvent implements DomainEvent {
+abstract class AbstractDomainEvent implements DomainEvent
+{
 
-	/**
-	 * @var DomainObjectId
-	 */
-	protected $aggregate_id;
-	/**
-	 * @var ilDateTime;
-	 */
-	protected $occurred_on;
-	/**
-	 * @var int
-	 */
-	protected $initiating_user_id;
-
-
-	/**
-	 * The Aggregate this event belongs to.
-	 *
-	 * @return DomainObjectId
-	 */
-	public function getAggregateId(): DomainObjectId {
-		return $this->aggregate_id;
-	}
+    /**
+     * @var DomainObjectId
+     */
+    protected $aggregate_id;
+    /**
+     * @var ilDateTime;
+     */
+    protected $occurred_on;
+    /**
+     * @var int
+     */
+    protected $initiating_user_id;
 
 
-	/**
-	 * @return string
-	 *
-	 * Add a Constant EVENT_NAME to your class: Name it: [aggregate].[event]
-	 * e.g. 'question.created'
-	 */
-	public abstract function getEventName(): string;
+    /**
+     * AbstractDomainEvent constructor.
+     *
+     * @param DomainObjectId $aggregate_id
+     * @param ilDateTime     $occurred_on
+     * @param int            $initiating_user_id
+     */
+    protected function __construct(DomainObjectId $aggregate_id, ilDateTime $occurred_on, int $initiating_user_id)
+    {
+        $this->aggregate_id = $aggregate_id;
+        $this->occurred_on = $occurred_on;
+        $this->initiating_user_id = $initiating_user_id;
+    }
 
 
-	/**
-	 * @return ilDateTime
-	 */
-	public function getOccurredOn(): ilDateTime {
-		return $this->occurred_on;
-	}
+    /**
+     * The Aggregate this event belongs to.
+     *
+     * @return DomainObjectId
+     */
+    public function getAggregateId() : DomainObjectId
+    {
+        return $this->aggregate_id;
+    }
 
 
-	/**
-	 * @return int
-	 */
-	public function getInitiatingUserId(): int {
-		return $this->initiating_user_id;
+    /**
+     * @return string
+     *
+     * Add a Constant EVENT_NAME to your class: Name it: [aggregate].[event]
+     * e.g. 'question.created'
+     */
+    public abstract function getEventName() : string;
 
-	}
+
+    /**
+     * @return ilDateTime
+     */
+    public function getOccurredOn() : ilDateTime
+    {
+        return $this->occurred_on;
+    }
 
 
-	/**
-	 * @return string
-	 */
-	public function getEventBody(): string {
-		return json_encode($this);
-	}
+    /**
+     * @return int
+     */
+    public function getInitiatingUserId() : int
+    {
+        return $this->initiating_user_id;
+    }
+
+
+    /**
+     * @return string
+     */
+    abstract public function getEventBody() : string;
+
+
+    /**
+     * @param string $event_body
+     */
+    abstract protected function restoreEventBody(string $event_body) : void;
 
 
     /**
@@ -83,11 +105,14 @@ abstract class AbstractDomainEvent implements DomainEvent {
      *
      * @return mixed
      */
-    abstract public static function restore(
+    public static function restore(
         DomainObjectId $aggregate_id,
         int $initiating_user_id,
         ilDateTime $occurred_on,
         string $event_body
-    ) : AbstractDomainEvent;
-
+    ) : AbstractDomainEvent {
+        $restored = new static($aggregate_id, $occurred_on, $initiating_user_id);
+        $restored->restoreEventBody($event_body);
+        return $restored;
+    }
 }
